c5d2ebf41a9daaa952b18919cfbd8dd0107a00be
GhostBear <jeffxiong87@outlook.com>
2018-03-19 21:06:21 +0800

    update

--- CrmJobs.R
+++ CrmJobs.R
@@ -1,7 +1,10 @@
 #setwd("E:\\Desktop\\R Language\\Practice4R")
-Sys.setlocale(category="LC_ALL", locale = "chinese")
-install.packages("xlsx")
-library("xlsx")
+#Sys.setlocale(category="LC_ALL", locale = "chinese")
+
+if(!require("xlsx")){
+  install.packages("xlsx")
+  require("xlsx")
+  }
 jobs<-read.xlsx("data\\jobs.xlsx",1,encoding="UTF-8")
 
 jobs<-na.omit(jobs)
@@ -28,9 +31,12 @@ jobs$jd<-as.vector(jobs$jd)
 #str(jobs)
 
 
+if(!require(stringr))
+{
+  install.packages("stringr")
+  require(stringr)
+}
 
-install.packages("stringr")
-library(stringr)
 
 #company type
 jobs$companytype<-lapply(str_split(str_replace_all(jobs$companytype,"\\t",""),"\\|"),str_trim)
@@ -64,37 +70,118 @@ jobs[which(jobs$salaryPeriod=="年"),"baseMinSalary"]<-round(as.numeric(jobs[whi
 jobs[which(jobs$salaryPeriod=="年"),"baseMaxSalary"]<-round(as.numeric(jobs[which(jobs$salaryPeriod=="年"),"baseMaxSalary"])/12)
 
 #JD
-install.packages("jiebaR")
-library("jiebaR")
-cutter=worker()
+if(!require("jiebaR"))
+{
+  install.packages("jiebaR")
+  require("jiebaR")
+}
+
+cutter<-worker(stop_word = "Bin\\stopword.txt", user = "Bin\\jieba.dict.user.utf8",topn = 20)
 #jobs$segWords<-segment(jobs$jd,cutter)
 tmp<-str_c(jobs$jd,"")
+
 segWords<-segment(tmp,cutter)
 
 
 
-library(plyr)
+if(!require("plyr"))
+{
+  install.packages("plyr")
+  require("plyr")
+}
 tableWords<-count(segWords)
 
 
+if(!require("wordcloud2"))
+{
+  install.packages("wordcloud2")
+  require("wordcloud2")
+}
 
-install.packages("wordcloud2")
-install.packages("RColorBrewer")
-install.packages("slam")
-library("RColorBrewer")
-library("wordcloud2")
-windowsFonts(myFont=windowsFont("华文彩云"))
-wordcloud2(tableWords,size=1)
-#tableWords[order(tableWords$freq, decreasing = TRUE),]
-tableWords[order(tableWords$freq, decreasing = TRUE),]
+tableWords<-head(tableWords[order(tableWords$freq, decreasing = TRUE),],100)
 #write.csv2(tableWords,file="C:\\jeff\\R\\Practice4R\\Bin\\segWords.csv")  #乱码
-write.csv(tableWords,file="E:\\Desktop\\R Language\\Practice4R\\Bin\\segWords.csv",sep = ",")  #乱码
+#write.csv(tableWords,file="E:\\Desktop\\R Language\\Practice4R\\Bin\\segWords.csv",sep = ",")  #乱码
+wordcloud2(tableWords,size=1)
 
+
+
+#job title
+
+jobTitleSegwords<-segment(str_c(jobs$jobtitle,""),cutter)
+jobTitleSegTbl<-count(jobTitleSegwords)
+jobTitleSegTbl<-head(jobTitleSegTbl[order(jobTitleSegTbl$freq,decreasing = TRUE),],100)
+wordcloud2(jobTitleSegTbl,minSize = 10)
 
 
 #绘图分析
-library(vcd)
+
+#if(!require("vcd"))
+#{
+#  install.packages("vcd")
+#  require(vcd)
+#}
 #
-means<-aggregate(jobs$baseMaxSalary,by=list(jobs$location),FUN=mean)
-barplot(means$x,names.arg = means$Group.1)
-abline(a=20000)
+#means<-aggregate(jobs$baseMaxSalary,by=list(jobs$location),FUN=mean)
+#barplot(means$x,names.arg = means$Group.1)
+#abline(a=20000)
+
+if(!require("ggplot2"))
+{
+  install.packages("ggplot2")
+  require("ggplot2")
+}
+#职位数量和地区的关系
+ggplot(jobs,aes(x=location))+geom_bar(position ="stack")+labs(title='position="stack')
+
+#薪水和地区的关系
+meanS<-aggregate(jobs$baseMaxSalary+jobs$baseMinSalary,by=list(jobs$location),FUN=mean)
+colnames(meanS)<-c("location","avgSalary")
+#barplot(means$x,names.arg = means$Group.1)
+ggplot(meanS,aes(x=location,y=avgSalary,fill=location))+geom_bar(stat="identity")
+
+
+
+
+#薪水和企业类型的关系
+meanS_compType<-aggregate(jobs$baseMaxSalary+jobs$baseMinSalary,by=list(jobs$cType),FUN=mean)
+colnames(meanS_compType)<-c("cType","avgSalary")
+ggplot(meanS_compType,aes(x=cType,y=avgSalary,fill=cType))+geom_bar(stat="identity")
+
+
+
+
+#薪水和title的关系?
+
+
+(jobs$baseMaxSalary+jobs$baseMaxSalary)/2
+s_t_matrix<-data.frame(jobs$jobtitle,salary=(jobs$baseMaxSalary+jobs$baseMaxSalary)/2)
+#需要制作一个关键词和薪水对于的表格，并可以知道某个关键词下的薪水分布情况
+s_t_matrix$jobs.jobtitle<-as.vector(s_t_matrix$jobs.jobtitle)
+s_t_matrix$jobtitle_breakdown<-""
+
+
+titleBD<-function(name)
+{
+  
+  s_t_matrix[s_t_matrix$jobs.jobtitle==name,3]<-  segment(name,cutter)
+}
+
+lapply(s_t_matrix$jobs.jobtitle,FUN = titleBD)
+s_t_matrix$titleBreakdown<-segment(s_t_matrix$jobs.jobtitle,cutter)
+
+for(i in 1:nrow(s_t_matrix))
+{
+  s_t_matrix[i,3]<-list(segment(s_t_matrix[i,1],cutter))
+}
+str(s_t_matrix[1,c("jobs.jobtitle")])
+
+
+s_t_matrix[1,3]<-list(segment(s_t_matrix[1,1],cutter))
+
+
+#会“微信”开发的人，一般拿多少薪水
+wechat_Salary<-jobs[str_detect(jobs$jd,"微信"),c("jobtitle","baseMinSalary","baseMaxSalary")]
+summary(wechat_Salary$baseMaxSalary)
+
+#Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#0   12500   16000   16819   20000   83333 
